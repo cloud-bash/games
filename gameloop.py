25c507f2b667d62877102a12a9c7df4ca191065e
cloud-bash <79466037+cloud-bash@users.noreply.github.com>
2022-07-29 20:35:55 -0400

    ternary conditional statement
    added a condition in the input function to change the prompt

--- gameloop.py
+++ gameloop.py
@@ -1,9 +1,9 @@
 #!/bin/python
 
-def gameloop():
-  response = input("Do you want to play a game?")
+def gameloop(response=0):
+  response = input("Do you want to play a game?" if response == 0 else "Do you want to play again?")
   if response == "yes":
-    gameloop()
+    gameloop(response)
   else:
     print("GAME OVER")
 
